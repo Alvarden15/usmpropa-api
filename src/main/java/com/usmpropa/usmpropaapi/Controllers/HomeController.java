93cb7dc539e18e127b201c3a610a108eb6717ee6
Mackuin <mackuin_luis@hotmail.com>
2021-06-13 17:20:41 -0500

    ultimos cambios

--- src/main/java/com/usmpropa/usmpropaapi/Controllers/HomeController.java
+++ src/main/java/com/usmpropa/usmpropaapi/Controllers/HomeController.java
@@ -4,16 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-<<<<<<< HEAD
-
-@Controller
-public class HomeController
-{
-    private static final String INDEX="index";
-    @GetMapping("/")
-    public String index(Model model) {
-        return INDEX;
-=======
 @Controller
 public class HomeController {
     
@@ -22,6 +12,5 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
         return HOME_INDEX;
->>>>>>> c69e7ef7b9e2f1b07f22b88feac158223a455d31
     }
 }
